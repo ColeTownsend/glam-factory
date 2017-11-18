4a6abfc774ac5c7928f51992ed4dbe344269e289
Cole Townsend <cole@coletownsend.com>
2017-11-18 17:16:19 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import 'react-primitives';
+import glamorous, { ThemeProvider } from 'glamorous-primitives';
+
+const StyledView = glamorous.view({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+})
+
+const StyledText = glamorous.text({
+  color: 'pink',
+  fontSize: '20px'
+})
+
+class App extends React.Component {
+	render() {
+		return (
+      <StyledView>
+        <StyledText>Hello World</StyledText>
+      </StyledView>
+		);
+	}
+}
+
+export default App;
